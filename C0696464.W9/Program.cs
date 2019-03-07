c3da1f903c9dcb713ab99f277b46bb36641807d8
visualstudio <visualstudio@230-PC36>
2019-03-07 14:53:27 -0500

    Lab 5(Ravinder singh,Hardeep Singh)

--- C0696464.W9/Program.cs
+++ C0696464.W9/Program.cs
@@ -10,43 +10,36 @@ using System.Threading.Tasks;
 //MARCH 6, 2019
 namespace C0696464.W9
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
 
         }
-
+    }
         public class DelegateExercises
         {
-            public delegate int MyDelegate(int intValue);
+            public delegate int MyDelegate();
 
             
-          public int Method1(int intMethod1)
+          void Method1()
             {
-                return intMethod1 * 2;
+            System.Console.WriteLine("MyDelegate");
             }
 
-            public int Method2(int intMethod2)
-            {
-                return intMethod2 * 10;
-            }
+           
 
             public void Method3()
             {
                 MyDelegate myDelegate = new MyDelegate(Method1);
-                int result1 = myDelegate(10);
-                System.Console.WriteLine(result1);
-                myDelegate = new MyDelegate(Method2);
-                int result2 = myDelegate(10);
-                System.Console.WriteLine(result2);
+            myDelegate();
             }
         }
     }
-}
+
 
 
     //-------------------------------------------------------
