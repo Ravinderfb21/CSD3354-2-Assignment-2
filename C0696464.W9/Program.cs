1608cb5650815ade1b353da9abd43ebd6364d540
visualstudio <visualstudio@230-PC36>
2019-03-07 14:27:10 -0500

    lab 3 (ravinder singh,hardeep singh)

--- C0696464.W9/Program.cs
+++ C0696464.W9/Program.cs
@@ -23,7 +23,9 @@ namespace C0696464.W9
         public class DelegateExercises
         {
             public delegate void MyDelegate();
-            void Method1()
+
+            
+            void Method1(int i)
             {
                 Console.WriteLine("Method1");
                 Console.ReadLine();
