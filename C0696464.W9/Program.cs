a3806474dce0d7ef92f0f7001df072cf31fcbacc
visualstudio <visualstudio@230-PC36>
2019-03-07 13:53:49 -0500

    new lab1

--- C0696464.W9/Program.cs
+++ C0696464.W9/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Student Names: Ravinder Singh(c0696464), Hardeep Singh(c0703625)
+//CSD3354 SECTION2
+//ASSIGNMENT2
+//MARCH 6, 2019
 namespace C0696464.W9
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+    }
+
 }
