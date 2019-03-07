3d08b09357fce7a6d245c0a9e8991f34500f5853
visualstudio <visualstudio@230-PC36>
2019-03-07 15:33:07 -0500

    Lab-010-(Ravinder Singh, Hardeep Singh)

--- C0696464.W9/Program.cs
+++ C0696464.W9/Program.cs
@@ -10,10 +10,29 @@ using System.Threading.Tasks;
 //MARCH 6, 2019
 namespace C0696464.W9
 {
-    public delegate void MyDelegate();
-    public class DelegateExercises : MyDelegate
+    public class Program
     {
+        public static void Main()
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
+        }
+    }
+    public delegate void MyDelegate();
+    public class DelegateExercises
+    {
+        void Method1()
+        {
+            System.Console.WriteLine("Method1");
 
+        }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+            System.Console.WriteLine(myDelegate.ToString());
+        }
     }
 }
 
